0bd1248746328b4627258550353b88bc575c823a
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-23 16:24:42 +0100

    Add first structure header

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,1 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Comics Laravel</title>
-
-    @vite('resources/js/app.js')
-</head>
-<body>
-    <h1>Prova</h1>
-</body>
-</html>
+@extends ('layouts.header');
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Comics Laravel</title>
+
+    @vite('resources/js/app.js')
+</head>
+<body>
+    <header>
+        <div class="header-stripe"></div>
+        <div class="nav-wrapper">
+            <div class="container">
+                <nav>
+                    <div class="main-nav-logo">
+                        <img src="./img/dc-logo.png" alt="dc logo">
+                    </div>
+                    <div class="nav-list">
+                        <ul>
+                            <li >
+                                <a href=""></a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
