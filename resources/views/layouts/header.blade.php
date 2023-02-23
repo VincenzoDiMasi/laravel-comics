1052a1ffe1bd2919fd7285f558e07c6b7b3b48e3
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-02-23 17:24:01 +0100

    Add jumbotron button

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,7 +31,9 @@
                     
         </div>
         <div id="jumbotron">
-
+            <button class="current-series">
+                <strong>CURRENT SERIES</strong> 
+            </button>
         </div>
                 
      </header>
